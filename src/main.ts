fb5969263596c0fd91ffce3f0a7942f02ff1e82e
roywaswa <roywaswa.rw@gmail.com>
2022-08-21 21:51:33 +0300

    finish

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,54 @@
-import './style.css';
 
-// const container = document.getElementsByClassName('container')[0];
+const ratingLabels = document.querySelectorAll<HTMLLabelElement>('label');
+const btnsubmit = document.querySelector<HTMLButtonElement>('#submit');
+const ratingcard = document.querySelector<HTMLDivElement>('#ratingcard');
+const thankyoucard = document.querySelector<HTMLDivElement>('#thankyoucard');
 
-// const RateUs = `
-  
-// `
-// container.innerHTML = RateUs;
+let ratingValue: string | undefined
 
-console.log("Hello World!");
+function activateOption(label: HTMLLabelElement) {
+  ratingLabels.forEach(label => { 
+    label.parentElement?.classList.remove('active');
+    document.getElementById(label.htmlFor)?.setAttribute('checked', 'unchecked');
+  })
+  const Input = document.querySelector<HTMLInputElement>(`#${label.htmlFor}`);
+  Input?.setAttribute('checked', 'checked');
+  ratingValue = Input?.value
+  label.parentElement?.classList.add('active');
+}
+
+function submitRating() {
+  if (ratingValue) { 
+    const rating = document.querySelector<HTMLSpanElement>('#rating');
+    rating!.innerText = ratingValue;
+    ratingcard?.classList.add('hidden')
+    thankyoucard?.classList.remove('hidden')
+    ratingcard?.classList.remove('card')
+    thankyoucard?.classList.add('card')
+  } 
+  return false;
+}
+
+
+document.addEventListener('DOMContentLoaded', () => { 
+  ratingLabels.forEach(label => {
+    label.addEventListener('click', (e) => {
+      e.preventDefault();
+      activateOption(label);
+    } );
+  });
+  btnsubmit?.addEventListener('click', (e) => { 
+    e.preventDefault();
+    btnsubmit.setAttribute('disabled', 'true');
+    btnsubmit.setAttribute('class', 'disabled');
+    btnsubmit.innerText = 'Submitting...';
+    setTimeout(() => {
+      submitRating();
+    }, 1000);
+    // btnsubmit.removeAttribute('disabled');
+    // btnsubmit.classList.remove('disabled');
+  })
+})
+
 export { };
 
